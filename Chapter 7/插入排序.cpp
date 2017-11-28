0c3c5d8666691b69cda28cb3699e745da221ed28
WuDeluan <30927072+WuDeluan@users.noreply.github.com>
2017-11-28 20:35:22 +0800

    Create 插入排序.cpp

---
+++ Chapter 7/插入排序.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+typedef int ElemenType;
+void InsertionSort(ElemenType A[], int N)
+{
+	int j, P;
+	ElemenType Tmp;
+	for (P = 1; P < N; P++)
+	{
+		Tmp = A[P];
+		for (j == P; j > 0 && A[j - 1] > Tmp; j--)
+			A[j] = A[j - 1];
+		A[j] = Tmp;
+	}
+}
