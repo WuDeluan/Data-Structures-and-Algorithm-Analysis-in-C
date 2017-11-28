445ffbb36b7ebac73c54c4f39861e595a6a11b79
WuDeluan <30927072+WuDeluan@users.noreply.github.com>
2017-11-28 21:12:54 +0800

    Update 插入排序.cpp

--- Chapter 7/插入排序.cpp
+++ Chapter 7/插入排序.cpp
@@ -8,7 +8,7 @@ void InsertionSort(ElemenType A[], int N)
 	for (P = 1; P < N; P++)
 	{
 		Tmp = A[P];
-		for (j == P; j > 0 && A[j - 1] > Tmp; j--)
+		for (j = P; j > 0 && A[j - 1] > Tmp; j--)
 			A[j] = A[j - 1];
 		A[j] = Tmp;
 	}
