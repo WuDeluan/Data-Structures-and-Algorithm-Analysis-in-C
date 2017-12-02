9a61b667b779005dc1ffb9c639fd54144c3e650e
WuDeluan <30927072+WuDeluan@users.noreply.github.com>
2017-12-02 16:13:53 +0800

    Update 第四章 平衡二叉树.cpp

--- 第四章 平衡二叉树.cpp
+++ 第四章 平衡二叉树.cpp
@@ -106,3 +106,77 @@ AvlTree Insert(AvlTree T, ElementType X)
 
 	return T;
 }
+
+Position FindMin(AvlTree T)  
+{
+	if (T == NULL)
+		return NULL;
+	if (T->Left == NULL)
+		return T;
+	else
+		return FindMin(T->Right);
+}
+
+Position Delete(AvlTree T,ElementType X)
+{
+	Position Temp;
+	if (T == NULL)
+		return NULL;
+	else if (X < T->Element)
+	{
+		T->Left = Delete(T->Left, X);
+		if (Height(T->Right) - Height(T->Left) == 2)
+			//由于在左子树的删除导致在T处平衡被破坏  
+			if (Height(T->Right->Left) > Height(T->Right->Right))
+				//如果T->Right的左子树比右子树高，执行双旋转  
+				T = DoubleRotateWithRight(T);
+			else
+				//否则执行单旋转  
+				T = SingleRotateWithRight(T);
+	}
+	else if (X > T->Element)
+	{
+		T->Right = Delete(T->Right, X);
+		if (Height(T->Left) - Height(T->Right) == 2)
+			//由于在右子树的删除导致在T处平衡被破坏  
+			if (Height(T->Left->Right) > Height(T->Left->Left))
+				//如果T->Left的左子树比右子树要高,执行单旋转  
+				T = SingleRotateWithLeft(T);
+			else
+				//否则执行双旋转  
+				T = DoubleRotateWithLeft(T);
+	}
+	else 
+	{//正常的删除策略  
+		if (T->Left && T->Right)
+		{
+			Temp = FindMin(T->Right);
+			T->Element = Temp->Element;
+			T->Right = Delete(T->Right, T->Element);
+		}
+		else
+		{
+			Temp = T;
+			if (T->Left == NULL)
+				T = T->Right;
+			else if (T->Right == NULL)
+				T = T->Left;
+			free(Temp);
+		}
+	}	
+	if (T)
+		//最后更新高度,此处可能T已经被设置成了NULL，所以需要检测下  
+	    T->Height = Max(Height(T->Left), Height(T->Right)) + 1;
+
+	return T;
+}
+
+int main()
+{
+	AvlTree T = (AvlNode*)malloc(sizeof(AvlNode));
+	T = NULL;
+	int A[] = { 0,1,2,3,4,5,6,7,8,9 };
+	for (int i = 0; i < 10; i++)
+		T = Insert(T, A[i]);
+	return 0;
+}
