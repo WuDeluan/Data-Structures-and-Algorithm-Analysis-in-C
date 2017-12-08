cc8c14919e7c2adfe366ba25976b2bbd8ab37d25
WuDeluan <30927072+WuDeluan@users.noreply.github.com>
2017-12-08 14:58:38 +0800

    Update 希尔排序.cpp

--- Chapter 7/希尔排序.cpp
+++ Chapter 7/希尔排序.cpp
@@ -1,14 +1,19 @@
 #include <stdio.h>
+#include <math.h>
 
 typedef int ElementType;
 
-//原始希尔排序
-void ShellSort(ElementType A[], int N)
+/*
+  原始希尔排序
+  增量元素不互质，则小增量可能根本不起作用
+ */
+void ShellSort0(ElementType A[], int N)
 {
-	int i, j, k,Increment;
+	int i, j, Increment;
 	ElementType Tmp;
 	for(Increment = N / 2;Increment > 0;Increment /= 2)
-		for (i = Increment; i < N; i++)
+	{
+		for (i = Increment; i < N; i++)            //插入排序
 		{
 			Tmp = A[i];
 			for (j = i; j >= Increment; j -= Increment)
@@ -19,8 +24,63 @@ void ShellSort(ElementType A[], int N)
 					break;
 			A[j] = Tmp;
 		}
+	}
 }
 
-//Hibbard增量序列
-<img src="http://latex.codecogs.com/gif.latex?\frac{\partial J}{\partial \theta_k^{(j)}}=\sum_{i:r(i,j)=1}{\big((\theta^{(j)})^Tx^{(i)}-y^{(i,j)}\big)x_k^{(i)}}+\lambda \theta_k^{(j)}" />  
+/*Hibbard增量序列  
+  Dk = 2^k - 1 —— 相邻元素互质
+  {1，3，7，15，31，...}
+*/
+double log2(double input)
+{
+	return log(input) / log(2);
+}
 
+void ShellSort1(ElementType A[], int N)
+{
+	int i, j, k, Round, Increment;
+	ElementType Tmp;
+	for (Round = (int)log2(N); Round > 0; Round--)
+	{
+		Increment = (int)pow(2, Round) - 1;                //根据公式，计算Hibbard增量
+		for (i = Increment; i < N; i++)                    //插入排序
+		{
+			Tmp = A[i];
+			for (j = i; j >= Increment; j -= Increment)
+				if (Tmp < A[j - Increment])
+					A[j] = A[j - Increment];
+				else
+
+					break;
+			A[j] = Tmp;
+		}
+	}
+}
+
+/*Sedgewick增量序列
+  9 * 4^i - 9 * 2^i + 1 或 4^i - 3 * 2^i - 1
+  {1,5,19,41,109,...}
+*/
+void ShellSort2(ElementType A[], int N)
+{
+	int i, j, k, Increment;
+	ElementType Tmp;
+	int Sedgewick[] = { 929, 505, 209, 109, 41, 19, 5, 1, 0 };  // 这里只列出一小部分增量
+
+	for (k = 0; Sedgewick[k] >= N; k++)
+		;                               // 初始的增量Sedgewick[Si]不能超过待排序列长度 
+	for (Increment = Sedgewick[k]; Increment > 0; Increment = Sedgewick[++k])
+	{
+		for (i = Increment; i < N; i++)                    //插入排序
+		{
+			Tmp = A[i];
+			for (j = i; j >= Increment; j -= Increment)
+				if (Tmp < A[j - Increment])
+					A[j] = A[j - Increment];
+				else
+
+					break;
+			A[j] = Tmp;
+		}
+	}
+}
