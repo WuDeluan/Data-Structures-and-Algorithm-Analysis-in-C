8ad52e6abe369a8e7b77f503cea7d2cc3c4c7949
WuDeluan <30927072+WuDeluan@users.noreply.github.com>
2017-12-08 15:49:38 +0800

    Update 堆排序.cpp

--- Chapter 7/堆排序.cpp
+++ Chapter 7/堆排序.cpp
@@ -1,40 +1,47 @@
 /*
-   ①将关键字构造成树。
-   ②下滤形成初始堆。
+①将关键字构造成树。
+②下滤形成初始堆。
 */
 #include <stdio.h>
 #include <stdlib.h>
 #define LeftChild(i)(2 * (i) + 1)
+
 typedef int ElementType;
+void Swap(ElementType *a, ElementType *b)
+{
+	ElementType Tmp;
+	Tmp = *a;
+	*a = *b;
+	*b = Tmp;
+}
+
 void PercDown(ElementType A[], int i, int N)
 {
 	int Child;
 	ElementType Tmp;
 
-	for (Tmp = A[i]; LeftChild(i) < N; i = Child)
+	for (Tmp = A[i]; LeftChild(i) < N; i = Child)           //取出根节点存放的值
 	{
-		Child = LeftChild(i);
-		if (Child != N - 1 && A[Child + 1] > A[Child])
-			Child++;
-		if (Tmp < A[Child])
+		Child = LeftChild(i);                           //得到左孩子的值
+		if (Child != N - 1 && A[Child + 1] > A[Child])  //取左右孩子中较大的
+			Child++; 
+		if (Tmp < A[Child])      //下滤
 			A[i] = A[Child];
-		else
+		else                     //找到合适位置            
 			break;
 	}
-	A[i] = Tmp;
+	A[i] = Tmp;                      //赋值
 }
 
 void HeapSort(ElementType A[], int N)
 {
 	int i;
 	ElementType Tmp;
-	for (i = N / 2; i >= 0; i--)
+	for (i = N / 2; i >= 0; i--)     //从最后一个不为叶子的节点开始下滤
 		PercDown(A, i, N);
-	for (i = N - 1; i > 0; i--)
+	for (i = N - 1; i > 0; i--)   
 	{
-		Tmp = A[0];
-		A[0] = A[i];
-		A[i] = Tmp;
-		PercDown(A, 0, i);
+		Swap(&A[0], &A[i]);      //“删除”最大根
+		PercDown(A, 0, i);       //调整大根堆
 	}
 }
