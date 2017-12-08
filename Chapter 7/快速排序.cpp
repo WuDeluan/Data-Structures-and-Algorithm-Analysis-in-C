9f5813402f8216c6bffc05c3ba874fadaa78b5e0
WuDeluan <30927072+WuDeluan@users.noreply.github.com>
2017-12-08 19:41:08 +0800

    Update 快速排序.cpp

--- Chapter 7/快速排序.cpp
+++ Chapter 7/快速排序.cpp
@@ -1,9 +1,10 @@
-/*①选择枢轴
-  ②将枢轴与最后一个数交换（隐藏枢轴）
-  ③ i 从 Right-1 开始向前，j 从0开始向后。当 i 遇到比枢轴小的，停下，当 j 遇到比枢轴大的，停下。
-  ④当 i，j 都停下时，交换指向的元素。
-  ⑤当 i > j，退出循环，将枢轴与 i 指向的元素交换。
-  */
+/*
+①选择基准(使A[Left] < R[Center] < A[Right]，交换A[Center],A[Right-1]。
+② 令 i 从 Right-1 开始向前，j 从0开始向后。
+③当 i 遇到比基准小的，停下，当 j 遇到比基准大的，停下。
+④当 i，j 都停下时，交换指向的元素。
+⑤当 i > j，退出循环，将基准与 i 指向的元素交换。
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #define Cutoff 3
@@ -15,21 +16,6 @@ void Swap(ElementType *a, ElementType *b)
 	Tmp = *a;
 	*a = *b;
 	*b = Tmp;
-}
-
-ElementType Median3(ElementType A[], int Left, int Right)
-{
-	int Center = (Left + Right) / 2;
-	
-	if (A[Left] > A[Center])
-		Swap(&A[Left], &A[Center]);
-	if (A[Left] > A[Right])
-		Swap(&A[Left], &A[Right]);
-	if (A[Center] > A[Right])
-		Swap(&A[Center], &A[Right]);
-
-	Swap(&A[Center], &A[Right - 1]);
-	return A[Right - 1];
 }
 
 void InsertionSort(ElementType A[], int N)
@@ -45,17 +31,34 @@ void InsertionSort(ElementType A[], int N)
 	}
 }
 
+ElementType Median3(ElementType A[], int Left, int Right)
+{
+	int Center = (Left + Right) / 2;
+
+	if (A[Left] > A[Center])
+		Swap(&A[Left], &A[Center]);
+	if (A[Left] > A[Right])
+		Swap(&A[Left], &A[Right]);
+	if (A[Center] > A[Right])
+		Swap(&A[Center], &A[Right]);
+	//此时A[Left] <= A[Center] <= A[Right]
+	
+	Swap(&A[Center], &A[Right - 1]);     //将基准Pivot藏到右边;
+	//此时A[Right]一定大于A[Right-1],接下来只需要考虑A[Left+1] … A[Right-2]
+	return A[Right - 1];      //返回基准
+}
+
 void QSort(ElementType A[], int Left, int Right)
 {
 	int i, j;
 	ElementType Pivot;
 
-	if (Left + Cutoff <= Right)
+	if (Right - Left >= Cutoff)      //如果序列元素大于阈值，进入快速排序
 	{
 		Pivot = Median3(A, Left, Right);
 		i = Left;
 		j = Right - 1;
-		for (; ;)
+		for (; ;)               //将序列中比基准小的移到基准左边，大的移到右边
 		{
 			while (A[++i] < Pivot) {}
 			while (A[--j] > Pivot) {}
@@ -64,16 +67,21 @@ void QSort(ElementType A[], int Left, int Right)
 			else
 				break;
 		}
-		Swap(&A[i], &A[Right - 1]);
+		Swap(&A[i], &A[Right - 1]);//将基准换到正确的位置
 
-		QSort(A, Left, i - 1);
-		QSort(A, i + 1, Right);
+		QSort(A, Left, i - 1);     //递归解决左边
+		QSort(A, i + 1, Right);    //递归解决右边
 	}
 	else
-		InsertionSort(A + Left, Right - Left + 1);
+		InsertionSort(A + Left, Right - Left + 1); //元素太少，用简单排序
 }
 
 void QuickSort(ElementType A[], int N)
 {
 	QSort(A, 0, N - 1);
 }
+
+/*
+时间复杂度：O(NlogN)
+稳定性：不稳定
+*/
