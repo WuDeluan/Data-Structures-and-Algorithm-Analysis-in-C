e2318e80f54f4246085bc5313ffb022336b2d302
WuDeluan <30927072+WuDeluan@users.noreply.github.com>
2017-12-09 21:13:05 +0800

    Update 第五章 散列（平方探测法）.cpp

--- 第五章 散列（平方探测法）.cpp
+++ 第五章 散列（平方探测法）.cpp
@@ -7,7 +7,7 @@ typedef int Index;          // 散列地址类型
 typedef Index Position;     // 数据所在位置与散列地址是同一类型 
 
 // 散列单元状态类型，分别对应：有合法元素、空单元、有已删除元素
-typedef enum { Legitimate, Empty, Deleted } EntryType;
+typedef enum { Legitimate, Empty, Deleted } EntryType;   //注意这里使用逗号！！！
 
 // 散列表单元类型
 typedef struct HashEntry {
